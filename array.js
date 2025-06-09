8b7cbb4ceeffbc522ab14b94e443594c82f8719f
santanupaikaray23 <santanupaikaray1996@gmail.com>
2025-06-09 13:46:45 +0530

    add all changes

--- array.js
+++ array.js
@@ -14,3 +14,31 @@ shift > always remove the first value of the array
 var a = ['a','b','c',1]
 var b = [1,2,3]
 a+b
+
+var city = ["werr","dfgh","vbn","asdf",12,34,1,56,true,false,"sdfg",true,45]
+undefined
+city.indexOf('sdff')
+-1
+city.indexOf('werr')
+0
+city.indexOf('vbn')
+2
+city.indexOf('dfgh')
+1
+
+var city = ["werr","dfgh",['Red','Yellow',["Bmw","Audi","Skoda"],'Green'],"vbn","asdf"]
+
+var myarray = ["werr","dfgh",['Red','Yellow',["Bmw","Audi","Skoda"],'Green'],"vbn","asdf"]
+undefined
+myarray[0]
+'werr'
+myarray[2]
+(4) ['Red', 'Yellow', Array(3), 'Green']
+myarray[2][2]
+(3) ['Bmw', 'Audi', 'Skoda']
+myarray[2][2][2]
+'Skoda'
+myarray.flat()
+(8) ['werr', 'dfgh', 'Red', 'Yellow', Array(3), 'Green', 'vbn', 'asdf']0: "werr"1: "dfgh"2: "Red"3: "Yellow"4: Array(3)0: "Bmw"1: "Audi"2: "Skoda"length: 3[[Prototype]]: Array(0)5: "Green"6: "vbn"7: "asdf"length: 8[[Prototype]]: Array(0)
+myarray.flat(2)
+(10) ['werr', 'dfgh', 'Red', 'Yellow', 'Bmw', 'Audi', 'Skoda', 'Green', 'vbn', 'asdf']
---
+++ loop.js
@@ -0,0 +1,25 @@
+
+for 
+while
+do while
+for of
+Map
+filter
+
+/////////////////////////////
+for> generate series of value or help to iterate over Array
+var a = 15
+for(i=0;i<5;i++){
+    console.log(i)
+
+}
+
+for(var i=0;i<5;i++){
+    let a =10
+    console.log(a+i)
+}
+
+var city = ["London","Delhi","NewYork","Paris"]
+for(i=0;i<city.length;i++){
+    console.log(city[i])
+}
